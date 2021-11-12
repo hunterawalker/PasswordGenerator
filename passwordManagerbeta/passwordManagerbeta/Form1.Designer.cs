9f697ad4e8bb9e88cda41a94316b302fab991b69
Hunter Walker <71032787+hunterawalker@users.noreply.github.com>
2021-11-12 14:10:38 -0600

    Add files via upload

---
+++ passwordManagerbeta/passwordManagerbeta/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace passwordManagerbeta
+{
+	partial class PasswordManager
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.Password = new System.Windows.Forms.Label();
+			this.PasswordLengthLabel = new System.Windows.Forms.Label();
+			this.PasswordLength = new System.Windows.Forms.TrackBar();
+			this.CopyPassword = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.PasswordLength)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// Password
+			// 
+			this.Password.Font = new System.Drawing.Font("Leelawadee", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Password.Location = new System.Drawing.Point(209, 56);
+			this.Password.Name = "Password";
+			this.Password.Size = new System.Drawing.Size(366, 65);
+			this.Password.TabIndex = 0;
+			this.Password.Text = "PasswordLabel";
+			this.Password.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// PasswordLengthLabel
+			// 
+			this.PasswordLengthLabel.Font = new System.Drawing.Font("Leelawadee", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.PasswordLengthLabel.Location = new System.Drawing.Point(209, 140);
+			this.PasswordLengthLabel.Name = "PasswordLengthLabel";
+			this.PasswordLengthLabel.Size = new System.Drawing.Size(366, 65);
+			this.PasswordLengthLabel.TabIndex = 1;
+			this.PasswordLengthLabel.Text = "Password Length: 5";
+			this.PasswordLengthLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// PasswordLength
+			// 
+			this.PasswordLength.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.PasswordLength.Location = new System.Drawing.Point(216, 237);
+			this.PasswordLength.Name = "PasswordLength";
+			this.PasswordLength.Size = new System.Drawing.Size(359, 45);
+			this.PasswordLength.TabIndex = 2;
+			this.PasswordLength.Scroll += new System.EventHandler(this.PasswordLength_Scroll);
+			// 
+			// CopyPassword
+			// 
+			this.CopyPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.CopyPassword.Location = new System.Drawing.Point(216, 315);
+			this.CopyPassword.Name = "CopyPassword";
+			this.CopyPassword.Size = new System.Drawing.Size(359, 44);
+			this.CopyPassword.TabIndex = 3;
+			this.CopyPassword.Text = "Copy Password";
+			this.CopyPassword.UseVisualStyleBackColor = true;
+			this.CopyPassword.Click += new System.EventHandler(this.CopyPassword_Click);
+			// 
+			// PasswordManager
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.CopyPassword);
+			this.Controls.Add(this.PasswordLength);
+			this.Controls.Add(this.PasswordLengthLabel);
+			this.Controls.Add(this.Password);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Name = "PasswordManager";
+			this.Text = "Password Manager";
+			((System.ComponentModel.ISupportInitialize)(this.PasswordLength)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label Password;
+		private System.Windows.Forms.Label PasswordLengthLabel;
+		private System.Windows.Forms.TrackBar PasswordLength;
+		private System.Windows.Forms.Button CopyPassword;
+	}
+}
+
---
+++ passwordManagerbeta/passwordManagerbeta/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace passwordManagerbeta
+{
+
+	public partial class PasswordManager : Form
+	{
+		int currentPasswordLength = 0;
+		Random character = new Random();
+
+
+		private void passwordGenerator(int passwordLength)
+		{
+			String allCharacters = "QWERTYTUYIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnmm1234567890!$*";
+			String randomPassword = "";
+			for(int i=0; i< passwordLength; i++)
+			{
+				int randomNum = character.Next(0, allCharacters.Length);
+				randomPassword += allCharacters[randomNum];
+			}
+			Password.Text = randomPassword; 
+		}
+
+
+		public PasswordManager()
+		{
+			InitializeComponent();
+			PasswordLength.Minimum = 5;
+			PasswordLength.Maximum = 20;
+			passwordGenerator(5);
+		}
+
+
+		private void CopyPassword_Click(object sender, EventArgs e)
+		{
+			Clipboard.SetText(Password.Text);
+		}
+
+
+		private void PasswordLength_Scroll(object sender, EventArgs e)
+		{
+			PasswordLengthLabel.Text = "Password Length: " + PasswordLength.Value.ToString();
+			currentPasswordLength = PasswordLength.Value;
+			passwordGenerator(currentPasswordLength);
+		}
+	}
+}
